5f0c0b14a9ff139c743205c359c34ff9adf68031
Wuriyanto <wuriyanto48@yahoo.co.id>
2020-01-20 17:16:27 +0700

    improve image preporcessor

--- image/prepocessor.py
+++ image/prepocessor.py
@@ -8,22 +8,134 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
+class ImagePreporcessor(object):
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-IMAGE_DIR = os.path.join(BASE_DIR, 'data')
-MODEL_DIR = os.path.join(BASE_DIR, 'model/model.h5')
+    def __init__(self, *args, **kwargs):
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-'''
-save tensorflow trained model
-'''
-def save_model(model):
-    model.save(MODEL_DIR)
+        if 'image_dir' not in kwargs:
+            raise TypeError('image_dir must be provide')
+        if 'model_dir' not in kwargs:
+            raise TypeError('image_dir must be provide')
+        if 'image_height' not in kwargs:
+            raise TypeError('image_height must be provide')
+        if 'image_width' not in kwargs:
+            raise TypeError('image_width must be provide')
+        if 'batch_size' not in kwargs:
+            raise TypeError('batch_size must be provide')
+        if 'epochs' not in kwargs:
+            raise TypeError('epochs must be provide')
 
-'''
-load tensorflow trained model from disk
-'''
-def load_model():
-    return tf.keras.models.load_model(MODEL_DIR)
+        self.image_height = kwargs['image_height']
+        self.image_width = kwargs['image_width']
+        self.batch_size = kwargs['batch_size']
+        self.epochs = kwargs['epochs']
+
+        self.image_dir = os.path.join(BASE_DIR, kwargs['image_dir'])
+        self.model_dir = os.path.join(BASE_DIR, kwargs['model_dir'])
+
+    '''
+    count all image file from IMAGE_DIR
+    '''
+    def __count_files(self):
+        types = ('*/*.*.jpg', '*/*.*.jpeg')
+        files_grabbed = []
+        for files in types:
+            file_dir = os.path.join(self.image_dir, files)
+            files_grabbed.extend(glob.glob(file_dir))
+        return files_grabbed
+    
+    '''
+    get class names, return all class name model
+    '''
+    def __get_class_names(self):
+        '''
+        assuming folder structure is like this
+        /data:
+            /cat:
+                - cat.0.jpg
+                - cat.1.jpg
+            /dog:
+                - dog.0.jpg
+                - dog.1.jpg
+        '''
+        return np.array([item.name for item in pathlib.Path(self.image_dir).glob('*')])
+
+    '''
+    create tensorflow model
+    '''
+    def __create_model(self):
+
+        model = tf.keras.Sequential()
+
+        model.add(tf.keras.layers.Conv2D(16, 3, input_shape=(self.image_height, self.image_width, 3), padding='same'))
+        model.add(tf.keras.layers.Activation('relu'))
+        model.add(tf.keras.layers.MaxPooling2D())
+        model.add(tf.keras.layers.Dropout(0.2))
+
+        model.add(tf.keras.layers.Conv2D(32, 3, padding='same'))
+        model.add(tf.keras.layers.Activation('relu'))
+        model.add(tf.keras.layers.MaxPooling2D())
+
+        model.add(tf.keras.layers.Conv2D(64, 3, padding='same'))
+        model.add(tf.keras.layers.Activation('relu'))
+        model.add(tf.keras.layers.MaxPooling2D())
+        model.add(tf.keras.layers.Dropout(0.2))
+
+        model.add(tf.keras.layers.Flatten())
+
+        model.add(tf.keras.layers.Dense(512))
+        model.add(tf.keras.layers.Activation('relu'))
+
+        model.add(tf.keras.layers.Dense(10))
+        model.add(tf.keras.layers.Activation('softmax'))
+
+        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        return model
+    
+    '''
+    save tensorflow trained model
+    '''
+    def __save_model(self, model):
+        model.save(self.model_dir)
+
+    '''
+    train and save tensorflow model to disk
+    '''
+    def train_model(self):
+        # convert from uint8 to float32 in range [0,1].
+        image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255,
+                                                        rotation_range=45,
+                                                        width_shift_range=.15,
+                                                        height_shift_range=.15,
+                                                        horizontal_flip=True,
+                                                        zoom_range=0.5
+                                                        )
+
+        train_data_gen = image_generator.flow_from_directory(directory=str(pathlib.Path(self.image_dir)),
+                                                        batch_size=self.batch_size,
+                                                        shuffle=True,
+                                                        target_size=(self.image_height, self.image_width),
+                                                        classes=list(self.__get_class_names()),
+                                                        class_mode='sparse'
+                                                    )
+
+        # image_batch,image_label = next(train_data_gen)
+        # show_batch(image_batch, image_label)
+
+        model = self.__create_model()
+
+        STEP_PER_EPOCH = np.ceil(len(self.__count_files()) / self.batch_size)
+
+        model.fit(train_data_gen, steps_per_epoch=STEP_PER_EPOCH, epochs=self.epochs)
+        self.__save_model(model)
+
+    '''
+    load tensorflow trained model from disk
+    '''
+    def load_model(self):
+        print(self.__get_class_names())
+        return tf.keras.models.load_model(self.model_dir)
 
 '''
 get image from disk, and return as numpy array
@@ -39,26 +151,6 @@ def get_image_normal(src, new_size=(50, 50)):
     return img_array
 
 '''
-count all image file from IMAGE_DIR
-'''
-def count_files():
-    types = ('*/*.*.jpg', '*/*.*.jpeg')
-    files_grabbed = []
-    for files in types:
-        file_dir = os.path.join(IMAGE_DIR, files)
-        files_grabbed.extend(glob.glob(file_dir))
-    return files_grabbed
-
-CLASS_NAME = np.array([item.name for item in pathlib.Path(IMAGE_DIR).glob('*')])
-
-IMAGES_COUNT = len(count_files())
-BATCH_SIZE = 5
-IMAGE_HEIGHT = 150
-IMAGE_WIDTH = 150
-STEP_PER_EPOCH = np.ceil(IMAGES_COUNT / BATCH_SIZE)
-EPOCHS = 50
-
-'''
 show batch from train gen data
 '''
 def show_batch(image_batch, label_batch):
@@ -71,74 +163,14 @@ def show_batch(image_batch, label_batch):
     plt.tight_layout()
     plt.show()
 
-'''
-create tensorflow model
-'''
-def create_model(input_shape):
-
-    model = tf.keras.Sequential()
-
-    model.add(tf.keras.layers.Conv2D(16, 3, input_shape=input_shape, padding='same'))
-    model.add(tf.keras.layers.Activation('relu'))
-    model.add(tf.keras.layers.MaxPooling2D())
-    model.add(tf.keras.layers.Dropout(0.2))
-
-    model.add(tf.keras.layers.Conv2D(32, 3, padding='same'))
-    model.add(tf.keras.layers.Activation('relu'))
-    model.add(tf.keras.layers.MaxPooling2D())
-
-    model.add(tf.keras.layers.Conv2D(64, 3, padding='same'))
-    model.add(tf.keras.layers.Activation('relu'))
-    model.add(tf.keras.layers.MaxPooling2D())
-    model.add(tf.keras.layers.Dropout(0.2))
-
-    model.add(tf.keras.layers.Flatten())
-
-    model.add(tf.keras.layers.Dense(512))
-    model.add(tf.keras.layers.Activation('relu'))
-
-    model.add(tf.keras.layers.Dense(10))
-    model.add(tf.keras.layers.Activation('softmax'))
-
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    return model
-
-'''
-train and save tensorflow model to disk
-'''
-def train_model():
-    # convert from uint8 to float32 in range [0,1].
-    image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255,
-                                                    rotation_range=45,
-                                                    width_shift_range=.15,
-                                                    height_shift_range=.15,
-                                                    horizontal_flip=True,
-                                                    zoom_range=0.5
-                                                    )
-
-    train_data_gen = image_generator.flow_from_directory(directory=str(pathlib.Path(IMAGE_DIR)),
-                                                    batch_size=BATCH_SIZE,
-                                                    shuffle=True,
-                                                    target_size=(IMAGE_HEIGHT, IMAGE_WIDTH),
-                                                    classes=list(CLASS_NAME),
-                                                    class_mode='sparse'
-                                                )
-
-    # image_batch,image_label = next(train_data_gen)
-    # show_batch(image_batch, image_label)
-
-    model = create_model(input_shape=(IMAGE_HEIGHT, IMAGE_WIDTH, 3))
-
-    model.fit(train_data_gen, steps_per_epoch=STEP_PER_EPOCH, epochs=EPOCHS)
-    save_model(model)
-
-def test_model(model, img_path):
+def test_model(img_proc, img_path):
 
     class_names = ['Drum', 'Guitar']
+    model = img_proc.load_model()
 
-    image_to_search = get_image_normal(img_path, new_size=(IMAGE_HEIGHT, IMAGE_WIDTH))
+    image_to_search = get_image_normal(img_path, new_size=(img_proc.image_height, img_proc.image_width))
     image_to_search = np.expand_dims(image_to_search, 0)
-    #predictions = model.predict(test_images)
+
     predictions = model.predict(image_to_search)
     print(predictions)
     print(np.argmax(predictions))
@@ -150,13 +182,22 @@ def test_model(model, img_path):
     plt.show()
 
 def main():
-    
-    # print(model.summary())
+    BATCH_SIZE = 5
+    IMAGE_HEIGHT = 150
+    IMAGE_WIDTH = 150
+    EPOCHS = 50
 
-    model = load_model()
-    test_model(model, 'img1.jpg')
+    img_proc = ImagePreporcessor(image_dir='data', 
+                model_dir='model/model.h5', 
+                image_height=IMAGE_HEIGHT, 
+                image_width=IMAGE_WIDTH,
+                batch_size=BATCH_SIZE,
+                epochs=EPOCHS
+                )
 
-    # train_model()
+    # img_proc.train_model()
+
+    test_model(img_proc, 'img2.jpg')
 
 if __name__ == '__main__':
     main()
